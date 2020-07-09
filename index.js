f5470de1eb1efcdd5a551aa3d0553700d197ee19
robert vanarsdall <Rob@roberts-Mac.local>
2020-07-09 02:12:23 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const root = document.getElementById("root");
+const imageScript = `<img src="https://picsum.photos/200/300?random=REPLACEME"></img>`;
+const people = [
+  {
+    name: "Mike",
+    quote: "Being weird is the side effect of awesomeness.",
+  },
+  {
+    name: "Jason",
+    quote:
+      "I was at a funeral and the widow asked if I would say a word. I said 'plethora.' She said “Thanks, that means a lot.",
+  },
+  {
+    name: "Minnie",
+    quote: "I wondered why the baseball was getting bigger. Then it hit me",
+  },
+  {
+    name: "Tyson",
+    quote: "I was addicted to the hokey pokey, but I turned myself around.",
+  },
+  {
+    name: "Jordan",
+    quote: "I’m not indecisive. Unless you want me to be.",
+  },
+  {
+    name: "Stephanie",
+    quote: "Me: Did you get a haircut? Dad: No, I got them all cut.",
+  },
+  {
+    name: "Jonnie",
+    quote: "I’d tell a chemistry joke but I know I wouldn’t get a reaction",
+  },
+  {
+    name: "Julie",
+    quote: "I deserve a medal every day I don’t stab someone with a fork.",
+  },
+];
+
+// Create code that will map through the Array
+// Create a random number between 1-100
+// Generate the Random Image
+// Create innerHTML that will wrap the following information
+// Display the person's name in H2 tags
+// Place the person's quote below their name in P tags
+// Display that random image
---
+++ map.js
@@ -0,0 +1,15 @@
+//Mapping through an Array of Numbers
+
+let numberArray = [1, 4, 5, 2, 1, 5, 7];
+
+// using .map() multiply each number by 3
+
+// convert this to an arrow function but multiply by 4
+
+//maping over objects
+
+let objectArray = [
+  { dogname: "rover", age: 8 },
+  { dogname: "lady", age: 3 },
+];
+// FORMULA FOR DOG AGE: (dogAge - 2) * 4 + 21;
